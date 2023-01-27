8bb05b9491770231450d9a0af733a70f248ab2f4
poojasj2 <123473429+poojasj2@users.noreply.github.com>
2023-01-27 17:37:59 +0530

    Delete car-entity-metadata.ts

--- car/dashboard/store/car-entity-metadata.ts
+++
@@ -1,8 +0,0 @@
-import { EntityMetadataMap } from "@ngrx/data";
-import { CarDetails } from "./car-detail";
- 
-export const carEntityMetaData: EntityMetadataMap = {
-    CarDetail:{
-        selectId:(carDetail:CarDetails)=>carDetail.id,
-      }
-}
